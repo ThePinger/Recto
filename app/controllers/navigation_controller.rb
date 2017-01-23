75b4e4d4519a851f6a4b77fb3be46385bae553b0
Omar Emad <omaremadsabry@gmail.com>
2017-01-23 22:05:31 +0200

    Generating first controller (#7)

---
+++ app/controllers/navigation_controller.rb
@@ -0,0 +1,14 @@
+class NavigationController < ApplicationController
+  def home
+  end
+
+  def login
+  end
+
+  def about
+  end
+
+  def signup
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/home' => 'navigation#home'
+  get '/login' => 'navigation#login'
+  get '/about' => 'navigtion#about'
+  get '/signup' => 'navigation#signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
