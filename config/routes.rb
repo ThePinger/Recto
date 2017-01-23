148b68011ed26d231beb2dce3dd8cd964e66e996
ShadyTantawy <ShadyTantawy@protonmail.com>
2017-01-24 00:30:52 +0200

    [+] Added default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'navigation#home'
   get '/home' => 'navigation#home'
   get '/login' => 'navigation#login'
   get '/signup' => 'navigation#signup'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
