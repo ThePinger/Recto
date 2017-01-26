33423eb18c584465e40aac61e5a6bd20db8da934
ShadyTantawy <ShadyTantawy@protonmail.com>
2017-01-26 22:56:55 +0200

    [+] Sessions [1]
    Added sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'navigation#home'
   get '/home' => 'navigation#home'
-  get '/login' => 'navigation#login'
   get '/signup' => 'user#new'
   post '/signup' => 'user#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
