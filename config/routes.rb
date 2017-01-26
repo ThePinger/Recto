d9e8a8e607beb6183ced602b3f27c42044245ada
ShadyTantawy <ShadyTantawy@protonmail.com>
2017-01-26 20:23:51 +0200

    Fixed Error
    Error: undefined method users_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/home' => 'navigation#home'
   get '/login' => 'navigation#login'
   get '/signup' => 'user#new'
-  post 'signup' => 'user#create'
+  post '/signup' => 'user#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
