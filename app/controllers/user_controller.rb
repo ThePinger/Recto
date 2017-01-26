37ce45346f87c757346cbeab1fcfd8956bef80cb
ShadyTantawy <ShadyTantawy@protonmail.com>
2017-01-26 19:20:07 +0200

    [new] action in User_controller
    Changed signup action to new + updated routes.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,9 @@
 class UserController < ApplicationController
 
-  def signup
+  def new
     @user = User.new
   end
-  
+
   def create
 	  @user = User.new(user_params)
 	  if @user.save
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'navigation#home'
   get '/home' => 'navigation#home'
   get '/login' => 'navigation#login'
-  get '/signup' => 'user#signup'
-
+  get '/signup' => 'user#new'
+  post 'signup' => 'user#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
